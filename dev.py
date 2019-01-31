09ccab75f5b5f4c447d5378b8937a7853b21988e
Paul Melnikow <github@paulmelnikow.com>
2019-01-31 14:01:17 -0600

    Set up CircleCI (#4)

--- dev.py
+++ dev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import os
 import click
@@ -20,7 +20,7 @@ def cli():
 
 @cli.command()
 def init():
-    execute("pip install -r requirements_dev.txt")
+    execute("pip3 install -r requirements_dev_py3.txt")
 
 
 @cli.command()
