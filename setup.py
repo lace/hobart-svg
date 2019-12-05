5532986642732f80f0f6f3f1e9d858f983b89836
Paul Melnikow <github@paulmelnikow.com>
2019-12-05 16:04:21 -0500

    Require Python 3 (#15)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Artistic Software",
         "Topic :: Multimedia :: Graphics :: 3D Modeling",
         "Topic :: Scientific/Engineering :: Visualization",
