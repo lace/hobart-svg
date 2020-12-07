85f411c86185449269342c09eaf89f0601d6e8ba
Paul Melnikow <github@paulmelnikow.com>
2020-12-07 15:21:49 -0500

    Note incompatibility with Python 3.9 + misc package/CI cleanup (#40)
    See lace/tri-again#25

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-import importlib
 from setuptools import find_packages, setup
 
-with open("README.md") as f:
-    readme = f.read()
+version_info = {}
+exec(open("hobart_svg/package_version.py").read(), version_info)
 
-with open("requirements.txt") as f:
-    install_requires = f.read()
+
+def load(filename):
+    return open(filename, "rb").read().decode("utf-8")
+
 
 setup(
     name="hobart-svg",
-    version=importlib.import_module("hobart_svg").__version__,
+    version=version_info["__version__"],
     description="Render polygons, polylines, and mesh cross sections to SVG",
-    long_description=readme,
+    long_description=load("README.md"),
     long_description_content_type="text/markdown",
     author="Metabolize",
     author_email="github@paulmelnikow.com",
     url="https://github.com/lace/hobart-svg",
     packages=find_packages(),
-    install_requires=install_requires,
+    python_requires=[">=3.6", "<3.9"],
+    install_requires=load("requirements.txt"),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Other Audience",
